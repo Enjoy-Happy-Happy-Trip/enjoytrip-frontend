22c0f14144a199007f6a6d278fc83a04badc4152
김주현 <skydreamer21@naver.com>
2023-05-09 10:21:53 +0900

    feat: home.vue 생성

---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <h1>this is home page</h1>
+        <h1 v-if="this.$store.state.isLoggedIn" @click="GoToLogin">
+            안녕하세요 {{ this.$store.state.user.user_name }} 님
+        </h1>
+        <button v-else @click="GoToLogin">go to login</button>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: "HomeView",
+    components: {},
+    methods: {
+        GoToLogin() {
+            this.$router.push("/login");
+        },
+    },
+};
+</script>
