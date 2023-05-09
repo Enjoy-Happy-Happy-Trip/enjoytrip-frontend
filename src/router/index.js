e6220bd1722c78263b78865599a6e67fb71ddd05
김주현 <skydreamer21@naver.com>
2023-05-09 10:20:59 +0900

    chore: vue setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/login",
+        name: "login",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ "../views/login.vue"),
+    },
+    {
+        path: "/test1",
+        name: "test1",
+        component: () => import("../views/test1.vue"),
+    },
+    {
+        path: "/test2",
+        name: "test2",
+        component: () => import("../views/test2.vue"),
+    },
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes,
+});
 
-export default router
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        isLoggedIn: false,
+        user: null,
+    },
+    getters: {},
+    mutations: {
+        LOGIN(state, user) {
+            state.isLoggedIn = true;
+            state.user = user;
+        },
+        LOGGOUT(state) {
+            state.isLoggedIn = false;
+            state.user = null;
+        },
+    },
+    actions: {},
+    modules: {},
+});
