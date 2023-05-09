2d50a9b6fe461000f703dcea5b4d351ae542cbd8
김주현 <skydreamer21@naver.com>
2023-05-09 10:24:56 +0900

    feat: create nav vue

---
+++ src/components/nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "NavView",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
