150075d1fb6047e453a3628d409606527504fc88
sungjun <tjdwnsmm@naver.com>
2023-05-10 17:02:36 +0900

    refactor: navbar included in App.vue and View pages renamed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
     <div id="app">
+        <nav-view></nav-view>
         <router-view />
     </div>
 </template>
+<script>
+import NavView from "@/components/nav.vue";
 
+export default{
+  name: "App",
+  components : {
+    NavView,
+  },
+}
+</script>
 <style></style>
