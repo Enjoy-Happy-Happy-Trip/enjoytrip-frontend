423a7157f637e1feb9463de3a7ec764fbd6a329f
sungjun <tjdwnsmm@naver.com>
2023-05-10 17:12:48 +0900

    refactor: View pages component name changed in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/home.vue";
+import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        name: "home",
+        name: "HomeView",
         component: HomeView,
     },
     {
         path: "/login",
-        name: "login",
+        name: "LoginView",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ "../views/login.vue"),
+        component: () => import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
+    },
+    {
+        path: "/myplan",
+        name: "MyPlanView",
+        component: MyPlanView,
     },
 ];
 
