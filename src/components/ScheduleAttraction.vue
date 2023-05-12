60bc4ff26f0f3bf687df758e7809de491ada4a76
sungjun <tjdwnsmm@naver.com>
2023-05-12 11:09:33 +0900

    refactor: Navigation Bar component renamed, ScheduleAttraction component added

---
+++ src/components/ScheduleAttraction.vue
@@ -0,0 +1,16 @@
+<template>
+    <tr>
+        <th v-text="attraction.title"></th>
+        <td><img :src="attraction.firstImage" alt="이미지 파일 없음"></td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: "ScheduleAttraction",
+    props: {
+        attraction: {},
+        index: Number,
+    }
+}
+</script>
