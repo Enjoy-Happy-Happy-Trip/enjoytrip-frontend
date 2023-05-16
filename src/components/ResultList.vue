45311a7ed34703cf617544b314371f3eb3f0e095
sungjun <tjdwnsmm@naver.com>
2023-05-16 16:33:30 +0900

    refactor: kakaomap and resultlist component extracted from tourinfoview

---
+++ src/components/ResultList.vue
@@ -0,0 +1,36 @@
+<template>
+    <tr @click="moveCenter(place.latitude, place.longitude)">
+        <td class="content-id" style="display: none">{{ place.contentId }}</td>
+        <td><img :src="place.firstImage" width="100px" /></td>
+        <td>{{ place.title }}</td>
+        <td>{{ place.addr }}</td>
+        <td>
+            <button :id="index" class="add-btn btn btn-warning">추가</button>
+        </td>
+        <td>
+            <button class="review-btn btn btn-warning scrollto">
+                Reivew 쓰기
+            </button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: "ResultList",
+    data() {
+        return {
+            linePath: [],
+        };
+    },
+    props: {
+        place: {},
+        index: Number,
+    },
+    methods: {
+        moveCenter(latitude, longitude) {
+            this.$emit("move-center", latitude, longitude);
+        }
+    },
+};
+</script>
