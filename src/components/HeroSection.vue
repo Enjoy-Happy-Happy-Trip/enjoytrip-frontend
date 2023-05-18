aee7821a9423800c1cc8b544abb19206bcfeab08
김주현 <skydreamer21@naver.com>
2023-05-18 17:05:07 +0900

    feat: create HeroSection component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="hero hero-inner">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 mx-auto text-center">
+                    <div class="intro-wrap">
+                        <h1 class="mb-0">{{ title }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroSection",
+    components: {},
+    props: {
+        title: String,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
