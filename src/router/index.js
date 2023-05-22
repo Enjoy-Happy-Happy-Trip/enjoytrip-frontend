d4819919d2d9ba80c84b1e3a21821d36c5733b5f
김주현 <skydreamer21@naver.com>
2023-05-22 11:29:36 +0900

    feat: add RegisterView to router and fix link in LoginView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 import MyScheduleView from "@/views/MyScheduleView.vue";
 import ScheduleDetail from "@/components/schedule/ScheduleDetail.vue";
 import ScheduleList from "@/components/schedule/ScheduleList.vue";
@@ -39,6 +40,11 @@ const routes = [
         path: "/",
         name: "HomeView",
         component: HomeView,
+    },
+    {
+        path: "/register",
+        name: "RegisterView",
+        component: RegisterView,
     },
     {
         path: "/plan",
