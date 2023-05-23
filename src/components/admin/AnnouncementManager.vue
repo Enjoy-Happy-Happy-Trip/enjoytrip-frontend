3b5b93aeebbce82c1aac16bca59d84fdb0977b4a
김주현 <skydreamer21@naver.com>
2023-05-23 17:09:09 +0900

    create Announce, memberManager admin pages

---
+++ src/components/admin/AnnouncementManager.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <hero-section title="공지사항 관리"></hero-section>
+    </div>
+</template>
+
+<script>
+import HeroSection from "@/components/HeroSection.vue";
+
+export default {
+    name: "AnnouncementManager",
+    components: {
+        HeroSection,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
