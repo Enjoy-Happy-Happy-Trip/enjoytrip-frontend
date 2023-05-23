74e719ae39b3f846c74c240b8424901007df8087
김주현 <skydreamer21@naver.com>
2023-05-23 16:05:18 +0900

    feat: create Admin View

---
+++ src/views/AdminView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <hero-section title="Admin Page"></hero-section>
+    </div>
+</template>
+
+<script>
+import HeroSection from "@/components/HeroSection.vue";
+
+export default {
+    name: "AdminView",
+    components: {
+        HeroSection,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {},
+    methods: {},
+};
+</script>
+
+<style scoped></style>
