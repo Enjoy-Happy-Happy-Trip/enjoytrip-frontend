9d48d62af185e8e96aac6f13244efa4acbf442b5
sungjun <tjdwnsmm@naver.com>
2023-05-25 16:15:46 +0900

    refactor: project name, footer text changed

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -6,16 +6,15 @@
                     <!-- Left side content -->
                     <h3>About Us</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam ac lectus vitae ex iaculis ullamcorper eget id
-                        mauris.
+                        HaoHao Trip is designed for users to easily create, share, and recommend their tour plans.
+                        HaoHao means "Good Good" in Chinese, and our title name implies that we hope the best for the users of our service. 
                     </p>
                 </div>
                 <div class="col-md-6">
                     <!-- Right side content -->
                     <h3>Contact Us</h3>
-                    <p>Email: example@example.com</p>
-                    <p>Phone: 123-456-7890</p>
+                    <p>Address: 서울 강남구 테헤란로 212 멀티캠퍼스 역삼</p>
+                    <p>Tel: 02-3429-5114</p>
                 </div>
             </div>
         </div>
